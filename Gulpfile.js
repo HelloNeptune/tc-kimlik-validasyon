eaf7f24a9bb221cc5dcc8dea404ec4b420bf2a78
Burak Abdikoglu <burak.abdikoglu@medyasoft.com.tr>
2017-06-14 18:14:19 +0300

    Live reload server implemented

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,8 +3,31 @@
  * 
  *  The dependencies
  */
-const gulp = require( 'gulp' );
+const gulp = require( 'gulp' )
+    , livereload = require( 'gulp-server-livereload' );
 
+/**
+ * 
+ *  Variables
+ */
+const watchfiles = [ './src/tckvalid.js', './src/index.html' ]
+    , serveropts = { livereload: true, open: true }
+    , serverport = 8181;
+
+
+
+/**
+ * @desc
+ *
+ * [ Gulp task: start-reload-server ]
+ */
+gulp.task( 'start-reload-server', () => {
+
+    // Start livereload server
+    // #
+    gulp.src( './src' )
+        .pipe(livereload( serveropts ));
+});
 
 /**
  * @desc It's default task of gulp automation
