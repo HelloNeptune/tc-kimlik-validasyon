31a5f47bf7d4ba64c94fc8f3f8978be5364dd7b1
Burak Abdikoglu <burak.abdikoglu@medyasoft.com.tr>
2017-06-14 15:27:10 +0300

    Added gulpfile into the project and implemented base syntax

---
+++ src/Gulpfile.js
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ *  The dependencies
+ */
+const gulp = require( 'gulp' );
+
+
+/**
+ * @desc It's default task of gulp automation
+ *
+ * [ Gulp task: default ]
+ */
+gulp.task( 'default', () => {
+
+    console.log( "Gulp working perfectly! ");
+});
